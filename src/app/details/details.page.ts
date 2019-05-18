6ae17af523f868dd2843d7f898ab2db21e8ffcc7
fabbau <bauer.fa@gmail.com>
2019-05-18 14:30:03 +0200

    initial commit 2

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { EventResponse, EmergencyEvent, Acknowledgement } from '../interfaces';
+import { ActivatedRoute } from '@angular/router';
+import { EventsService } from '../events.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+  eventId: number;
+  eventResponse: EventResponse
+  event: EmergencyEvent;
+  acknowledgement: Acknowledgement[] = [];
+  newNote = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private eventService: EventsService
+  ) { }
+
+  async ngOnInit() {
+    this.eventId = +this.route.snapshot.params['eventId'];
+    this.eventResponse = await this.eventService.getById(this.eventId).toPromise();
+    this.event = this.eventResponse.event;
+    this.acknowledgement = await this.eventService.getAcknowledgements(this.eventResponse)
+      .toPromise();
+  }
+}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventResponse } from '../interfaces';
+import { Subscription } from 'rxjs';
+import { EventsService } from '../events.service';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  sub: Subscription;
+
+  constructor(private eventService: EventsService,
+    private nav: NavController) {  }
+
+  ngOnInit(): void {
+    this.sub = this.eventService.getAll()
+      .subscribe(e => this.events.push(e));
+    // throw new Error("Method not implemented.");
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    // throw new Error("Method not implemented.");
+  }
+
+  getEvents(): EventResponse[] {
+    return this.events.sort( (a,b) => a.event.created > b.event.created ? -1 : 1);
+  }
+
+  details(response: EventResponse) {
+    this.nav.navigateForward(`/details/${response.event.id}`)
+  }
+}
